6ec263256ebd0d6869a693276a0611fa53f95c26
ksby6974 <yswtkddn6479@naver.com>
2024-10-30 15:48:06 +0900

    241030
    .

--- Pro_eyelash/Assets/Scripts/Manager/TitleManager.cs
+++ Pro_eyelash/Assets/Scripts/Manager/TitleManager.cs
@@ -6,17 +6,25 @@ using TMPro;
 
 public class TitleManager : MonoBehaviour
 {
-
+    [SerializeField] TextMeshProUGUI [] textPros;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetMenuText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetMenuText()
+    {
+        textPros[0].text = "START";
+        textPros[1].text = "LIBRARY";
+        textPros[2].text = "OPTION";
+        textPros[3].text = "EXIT";
     }
 }
